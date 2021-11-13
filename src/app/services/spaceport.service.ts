0e078f2a439f5c4609fdf34c455a5d195547c610
mgvalenzu <51139227+mgvalenzu@users.noreply.github.com>
2021-11-12 23:26:29 -0800

    Add addSpaceport() Service

--- src/app/services/spaceport.service.ts
+++ src/app/services/spaceport.service.ts
@@ -10,7 +10,14 @@ export class SpaceportService {
 
   constructor(private http: HttpClient) { }
 
+  private postHeaders = new HttpHeaders({ 'Context-Type': 'application/json' });
+
   getAllSpaceports(): Observable<Spaceport[]>{
     return this.http.get<Spaceport[]>('http://localhost:8080/spaceports');
   }
+
+  addSpaceport(spaceport: Spaceport): Observable<Spaceport> {
+    return this.http.post<Spaceport>('http://localhost:8080/spaceports', spaceport, { headers: this.postHeaders });
+  }
+
 }
