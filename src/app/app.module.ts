5590a3413c79984116ce2e3c2dc227dfad1f20fe
mgvalenzu <51139227+mgvalenzu@users.noreply.github.com>
2021-11-12 23:29:54 -0800

    CreateDestination Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RegisterService } from './services/register.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
 import { VendorhomeComponent } from './components/vendorhome/vendorhome.component';
 import { TicketComponent } from './components/ticket/ticket.component';
-
 import { CustomerHomeComponent } from './components/customer-home/customer-home.component';
-
 import { FlightListComponent } from './flight-list/flight-list.component';
 import { ChildFindFlightsComponent } from './child-find-flights/child-find-flights.component';
-
 import { RegisterComponent } from './components/register/register.component';
 import { VendorregisterComponent } from './components/vendorregister/vendorregister.component';
-import { RegisterService } from './services/register.service';
 import { CustomerLandingComponent } from './components/customerLanding/customer-landing.component';
 import { VendorCreateFlightsComponent } from './components/vendor-create-flights/vendor-create-flights.component';
 import { VendorflightviewComponent } from './components/vendorflightview/vendorflightview.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { GuestComponent } from './components/guest/guest.component';
 import { ConfirmedComponent } from './components/confirmed/confirmed.component';
+import { CreateDestinationComponent } from './components/create-destination/create-destination.component';
+
+
 
 
 
@@ -45,7 +45,7 @@ import { ConfirmedComponent } from './components/confirmed/confirmed.component';
     LogoutComponent,
     GuestComponent,
     ConfirmedComponent
-
+    CreateDestinationComponent
 
   ],
   imports: [
